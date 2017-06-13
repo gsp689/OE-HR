c304d8bd2111a8080c85a6135365ce734b9209eb
gsp689 <gsp689@gmail.com>
2017-06-13 14:27:09 +0530

    first commit

---
+++ OE-HR/src/com/oe/test/TestClass.java
@@ -0,0 +1,17 @@
+package com.oe.test;
+
+import java.io.IOException;
+import java.text.ParseException;
+import com.oe.services.AttendanceFileOperator;
+import com.oe.services.AttendanceFileOperatorForOld;
+import com.oe.services.AttendanceFileOperatorV1;
+import com.oe.services.XlsWriter;
+
+public class TestClass {
+	
+	public static void main(String[] args) throws IOException, ParseException {
+	
+		String s = null;
+		System.out.println(s.equals(""));
+	}
+}
